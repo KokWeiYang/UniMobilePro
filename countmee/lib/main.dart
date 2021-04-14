4c9cfe7ab0e22b3b9647497fb0ba9317735e8aca
KokWeiYang <weiyangkok@gmail.com>
2021-04-14 22:15:02 +0800

    hello

--- countmee/lib/main.dart
+++ countmee/lib/main.dart
@@ -7,6 +7,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Material App', home: SplashScreen());
+    return MaterialApp(title: 'Login App', home: SplashScreen());
   }
 }
